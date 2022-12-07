c93368f6d59c0bb20e888129a28b42676775f2c6
Evander Fernandes <evanderfernandes@live.com>
2022-12-07 17:18:23 +0530

    feat: Added order.API and setup base structure to implement Clean Architecture.

---
+++ src/Services/Order/Order.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+using Order.Application.Features.Orders.Queries.GetOrdersList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerOrder = Order.Domain.Entities.Order;
+
+namespace Order.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CustomerOrder, OrdersVm>().ReverseMap();
+            CreateMap<CustomerOrder, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<CustomerOrder, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
